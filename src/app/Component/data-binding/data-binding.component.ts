8d9d65a289df5acc5fe6a271945c962d8c2acc30
namanydv <namanofficial91@gmail.com>
2025-12-31 11:46:45 +0530

    feat: enhance data binding component with additional inputs and styling

--- src/app/Component/data-binding/data-binding.component.ts
+++ src/app/Component/data-binding/data-binding.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
-  styleUrl: './data-binding.component.css'
+  styleUrl: './data-binding.component.css',
 })
 export class DataBindingComponent {
+  // string, number, boolean, data
 
+  courseName: string = 'Angular 20';
+  isActive: boolean = true;
+  currentDate: Date = new Date();
+  rollNo: number = 2350958113;
+  spanClassNAme = 'secondary'
+
+  maxTestLength = 12;  
+
+  constructor() {
+    console.log(this.courseName);
+
+    this.courseName = 'Angular Tutorial';
+    console.log(this.courseName);
+
+    setTimeout(()=>{
+      this.rollNo = 123456789;
+    },10000) 
+  }
 }
