4c1ce5e7c989e349a91433dc3822899a83240174
namanydv <namanofficial91@gmail.com>
2025-12-31 13:08:57 +0530

    feat: add Signal component with template, styles, and unit tests

---
+++ src/app/Component/signal/signal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal',
+  imports: [],
+  templateUrl: './signal.component.html',
+  styleUrl: './signal.component.css'
+})
+export class SignalComponent {
+
+}
